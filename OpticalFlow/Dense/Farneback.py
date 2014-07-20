06771a22b5a60a4a1d5df3c9e596b206468a9da3
kostas <konst.makantasis@gmail.com>
2014-07-20 22:45:46 +0300

    Optical flow techniques

--- OpticalFlow/Dense/Farneback.py
+++ OpticalFlow/Dense/Farneback.py
@@ -6,6 +6,39 @@ import cv2
 
 class Farneback(object):
     def __init__(self,  pyr_scale=0.5, levels=3, winsize=15, iterations=3, poly_n=1.2, poly_sigma=0):
+        """               
+            Function definition
+            +++++++++++++++++++
+            
+            .. py:function:: __init__(pyr_scale=0.5, levels=3, winsize=15, iterations=3, poly_n=1.2, poly_sigma=0)
+
+                Initializes the object that computes a dense optical flow using the Gunnar Farneback’s algorithm.
+
+                :param float pyr_scale: scaling factor between images in a Gaussian pyramid is subsequent levels.
+                :param int levels: levels of the Gaussian pyramid.
+                :param int winsize: averaging window size; larger values increase the algorithm robustness 
+                                    to image noise and give more chances for fast motion detection, but 
+                                    yield more blurred motion field.
+                :param int iterations: number of iteration the algorithm does at each pyramid level.
+                :param float poly_n: size of the pixel neighborhood used to find polynomial expansion in 
+                                     each pixel; larger values mean that the image will be approximated 
+                                     with smoother surfaces, yielding more robust algorithm and more 
+                                     blurred motion field, typically poly_n =5 or 7.
+                :param float poly_sigma: standard deviation of the Gaussian that is used to smooth 
+                                         derivatives used as a basis for the polynomial expansion; for 
+                                         poly_n=5, you can set poly_sigma=1.1, for poly_n=7, a good value 
+                                         would be poly_sigma=1.5.
+                
+            **Outputs - Class members**:
+            
+            * motion_image: an 3-d numpy array containing the motion image. The value of each pixel is \
+                            assigned using the magnitude and the direction of the motion.
+            * magnitude_image: an 2-d numpy array containing the magnitude of the motion at each one \
+                               pixel normalized in :math:`[0,255]`.
+            * direction_image: an 2-d numpy array containing the direction of the motion at each one \
+                               pixel in degrees.
+               
+        """
         
         self.pyr_scale = pyr_scale
         self.levels = levels
@@ -16,19 +49,35 @@ class Farneback(object):
         
         self.motion_image = None
         self.magnitude_image = None
+        self.direction_image = None
         
     
     def CalculateOpticalFlow(self, prev, nxt):
+        """               
+            Function definition
+            +++++++++++++++++++
+            
+            .. py:function:: CalculateOpticalFlow(prev, nxt)
+            
+                Computes a dense optical flow using the Gunnar Farneback’s algorithm using two subsequent
+                frames.
+
+                :param numpy_array prev: the first frame of the two subsequent frames.
+                :param numpy_array nxt: the second frame of the two subsequent frames.
+                             
+        """
         
         hsv = np.zeros((prev.shape[0], prev.shape[1], 3))
         hsv[...,1] = 255
         
         flow = cv2.calcOpticalFlowFarneback(prev,nxt, 0.5, 3, 15, 3, 5, 1.2, 0)
+        self.flow = flow
 
-        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-        hsv[...,0] = ang*180/np.pi/2
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1], angleInDegrees=1)
+        hsv[...,0] = ang/2
         hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
         hsv = np.array(hsv, dtype=np.uint8)
         self.motion_image = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
         self.magnitude_image = hsv[...,2]
+        self.direction_image = ang
 
--- OpticalFlow/Dense/kmFarneback_demo.py
+++ OpticalFlow/Dense/kmFarneback_demo.py
@@ -3,20 +3,61 @@
 import cv2
 import numpy as np
 import scipy.io as sio
+from scipy import ndimage
+from scipy.spatial.distance import pdist
+#from scipy.cluster.vq import kmeans,vq
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import Farneback
+import Morphology
+import AgglomerativeClustering
 
 
 def Img2Gray(img):
     img = img - np.min(img)
     img = img / np.max(img)
     img = img * 255
-    img = np.array(img, dtype=np.uint8)
+    img = np.array(img, dtype=np.uint16)
     
     return img
     
+  
+def ClusterObjects(farn, struct_elem):
+    magn_img = farn.magnitude_image
+    dir_img = farn.direction_image    
     
+    bin_img = np.zeros(shape=(magn_img.shape[0], magn_img.shape[1]), dtype=np.uint8)
+    bin_img[magn_img < 25] = 0
+    bin_img[magn_img >= 25] = 1
+    
+    bin_img = ndimage.binary_dilation(bin_img, structure=struct_elem, iterations=3).astype(bin_img.dtype)
+
+    labels, nb_labels = Morphology.ConnenctedComponents(bin_img)    
+    filt_labels, areas, nb_new_labels = Morphology.FilterArea(bin_img, labels, nb_labels, 480)
+    
+    
+    temp_magn = ndimage.mean(magn_img, filt_labels, range(nb_new_labels + 1))
+    temp_dir = ndimage.mean(dir_img, filt_labels, range(nb_new_labels + 1))
+    
+    data = np.concatenate((np.reshape(temp_magn, (-1,1)), np.reshape(temp_dir, (-1,1))), axis=1)
+    
+    clusters = -1
+    if nb_new_labels >= 1:
+        Y = pdist(data, 'euclidean')
+        agglo = AgglomerativeClustering.Agglomerative(Y, 50.)
+        agglo.AggloClustering(criterion = 'distance', method = 'single', metric = 'euclidean', normalized = False)
+
+        clusters = agglo.clusters
+             
+    bin_img[filt_labels == 0] = 0
+    bin_img[filt_labels >= 1] = 1
+    
+    
+    
+    return bin_img, nb_new_labels, temp_magn, temp_dir, data, clusters
+    
+    
+  
 
 data = sio.loadmat('../../data/Classified/AIA_mac0001.SEQ_1.mat')
 thermal_cube = data['thermal_cube']
@@ -32,7 +73,11 @@ prvs = Img2Gray(img)
 farn = Farneback.Farneback()
 
 fig = plt.figure()
+ax1=fig.add_subplot(1,2,1)
+ax2=fig.add_subplot(1,2,2)
 ims = []
+
+struct_elem = ndimage.generate_binary_structure(2, 1)
 
 for i in range(n_images):
     frame2 = np.transpose(thermal_cube[:,:,i])
@@ -41,6 +86,14 @@ for i in range(n_images):
 
     farn.CalculateOpticalFlow(prvs, nxt)
     rgb = farn.motion_image
+    
+    ######### Color Quantization #####################################
+    #temp_rgb = np.reshape(rgb,(rgb.shape[0]*rgb.shape[1],3))
+    #centroids,_ = kmeans(temp_rgb,6) 
+    #qnt,_ = vq(temp_rgb,centroids)  
+    #centers_idx = np.reshape(qnt,(rgb.shape[0],rgb.shape[1]))
+    #clustered = centroids[centers_idx]
+    ##################################################################
 
     cv2.imshow('frame2',rgb)
     k = cv2.waitKey(1) & 0xff
@@ -48,18 +101,18 @@ for i in range(n_images):
         break
     
     prvs = nxt
+
+    #temp, nb_new_labels, temp_magn, temp_dir, data, clusters = ClusterObjects(farn, struct_elem)
     
-    temp = np.array(farn.magnitude_image, dtype=np.float64)
-    temp[temp >=32] = np.max(frame2)
-    temp[temp < 32] = np.min(frame2)    
+    #temp2 = np.concatenate((frame2, temp), axis=1)        
     
-    temp2 = np.concatenate((frame2, temp), axis=1)        
-    
-    im = plt.imshow(temp2)
-    ims.append([im])
+    im1 = ax1.imshow(frame2)
+    im2 = ax2.imshow(rgb)
+    ims.append([im1, im2])
+        
+cv2.destroyAllWindows()
     
 ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=100)
-
+ani.save('../../results/dense_flow.mp4', fps=25, dpi=600)
 plt.show()
 
-cv2.destroyAllWindows()
